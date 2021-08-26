0133bb39fecb54ae27b2e42b89aafd3fbd601b36
gubing <sdhg12@naver.com>
2021-08-26 21:43:34 +0900

    init make gatsbypage

---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+//gatesby develop
+module.exports = {
+    plugins: [
+      {
+        resolve: `gatsby-theme-blog`,
+        options: {
+          // basePath defaults to `/`
+        },
+      },
+    ],
+    siteMetadata: {
+      title: `Hello, Gatsby!`,
+      social: {},
+    }
+  }
